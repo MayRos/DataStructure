25825ef30a0efcb4d9b2a85c198e4e9e8131e510
MayRos <3270728477@qq.com>
2022-03-17 19:35:53 +0800

    看了一遍，下次再做

--- leecode/432.cpp
+++ leecode/432.cpp
@@ -2,106 +2,27 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <map>
+#include <unordered_set>
 using namespace std;
-struct MyListNode {
-    string key;
-    int val;
-    MyListNode* next;
-    MyListNode* last;
-    MyListNode() : key(""),val(0), next(nullptr),last(nullptr) {}
-};
+//using namespace std;
 class AllOne {
+    unordered_map<string, list<pair<unordered_set<string>, int>>::iterator> m;
+    list<pair<unordered_set<string>, int>> list;
 public:
-    MyListNode* head = new MyListNode;
-    MyListNode* tail = new MyListNode;
-    MyListNode* ptr = head;
-    AllOne() {
-        head->next = tail;
-        tail->last = head;
-    }
+    AllOne() {}
 
     void inc(string key) {
-        find(key);
-        if ( ptr!= tail) {
-            ptr->val = ptr->val + 1;
-            while (ptr->next != tail && ptr->val > ptr->next->val )
-            {
-                MyListNode* pre = ptr->last;
-                MyListNode* next = ptr->next;
-                MyListNode* nextnext = next->next;
-                pre->next = next;
-                next->last = pre;
-                next->next = ptr;
-                ptr->last = next;
-                ptr->next = nextnext;
-                nextnext->last = ptr;
-            }
-        }
-        else {
-            MyListNode* thisPtr = new MyListNode;
-            thisPtr->key = key;
-            thisPtr->val = 1;
-            MyListNode* first = head->next;
-            head->next = thisPtr;
-            thisPtr->last = head;
-            thisPtr->next = first;
-            first->last = thisPtr;
-        }
     }
 
     void dec(string key) {
-        find(key);
-        ptr->val = ptr->val - 1;
-        if (ptr->val == 0) {
-            MyListNode* pre = ptr->last;
-            MyListNode* next = ptr->next;
-            pre->next = next;
-            next->last = pre;
-        }
-        else if (ptr->last != head && ptr->val < ptr->last->val) {
-            MyListNode* pre = ptr->last;
-            MyListNode* prepre = pre->last;
-            MyListNode* next = ptr->next;
-            prepre->next = ptr;
-            ptr->last = prepre;
-            ptr->next = pre;
-            pre->last = ptr;
-            pre->next = next;
-            next->last = pre;
-        }
     }
 
     string getMaxKey() {
-        if (tail->last == head) {
-            return "";
-        }
-        else {
-            return tail->last->key;
-        }
     }
 
     string getMinKey() {
-        if (head->next == tail)
-        {
-            return "";
-        }
-        else {
-            return head->next->key;
-        }
     }
 private:
-    void find(string key) {
-        ptr = head;
-        while (ptr != tail)
-        {
-            if (ptr->key == key) {
-                return;
-            }
-            else {
-                ptr = ptr->next;
-            }
-        }
     }
 };
 int main() {
